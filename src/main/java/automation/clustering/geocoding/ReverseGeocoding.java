ae0029406113914caeba72a1fd5da925c87f648f
skywalker <wsky3478@gmail.com>
2025-12-23 17:42:04 +0300

    add geocoding

---
+++ src/main/java/automation/clustering/geocoding/ReverseGeocoding.java
@@ -0,0 +1,7 @@
+package automation.clustering.geocoding;
+
+public class ReverseGeocoding {
+    public static String reverseGeocode(double latitude, double longitude) {
+        return "";
+    }
+}
